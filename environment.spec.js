e1b116ae457f58343475db9d0539a31bd00d487a
aminnairi <aminnairi@noreply.users.github.com>
2021-03-04 18:11:31 +0100

    :sparkles: creating and deleting a test env file

--- environment.spec.js
+++ environment.spec.js
@@ -1,6 +1,37 @@
-const {describe, it} = require("mocha");
+"use strict";
+
+const {describe, it,before, after} = require("mocha");
 const {expect} = require("chai");
 const {Environment} = require("./environment.js");
+const {promises: fs} = require("fs");
+before(async () => {
+    try {
+        await fs.writeFile(".env.example", `
+                # This is a comment
+                NAME=environment
+
+                # This is a multiline message actually
+                MESSAGE=This \\
+                            is \\
+                            a \\
+                            message
+
+                # This is a string that will be interpollated
+                INTERPOLLATION="The name of this package is $NAME"
+
+                # This string will never be interpollated
+                STATIC='The name of this package is $NAME'
+
+                # For the fancy ones
+                SPACED      =       Lots of spaces
+            `);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+after(async () => {
+});
 
 describe("environment", () => {
     const maybeEnvironment = Environment.from(".env.example");
